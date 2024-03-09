8d2be8523b0b6ba99c45e9093feeaeb3fcdbf005
Sferrazza Giovanni <78505462+Giosferrazza@users.noreply.github.com>
2024-03-09 12:55:49 -0800

    Added the first cpp file that includes notes

---
+++ youtube.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //input output stream is a preprocessor command
+
+using std::cout;
+
+int main() //main function int is short for integer related to return 0
+{
+    int slices = 5;
+    cout << slices; //statement or steps end with ;
+}
+
+
+// (<<) is an operator and means that you will operate on the following operand
+// cout === console out
+// cin  === console in
+// return 0; means the program ran well and returned a 0
