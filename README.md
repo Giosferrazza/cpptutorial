fc3f6988220d7cd77a74e8b465ba2d97068a799f
Sferrazza Giovanni <78505462+Giosferrazza@users.noreply.github.com>
2024-03-11 01:10:42 -0700

    Function review

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# cpptutorial
+# My C++ tutorial journey
+I have been programming in c++ for 5 years for school and never really wanted to learn it for myself I wanted to focus on web dev and python.
+
+Now I have decided to go all in on c++ and become an advanced programmer while still in school so when I am ready to move on to other languages I will be well informed of how things work on a lower level.
+
+If you are reading this welcome to my journey to becoming a software engineer!
--- youtube.cpp
+++ youtube.cpp
@@ -1,14 +1,32 @@
 #include <iostream> //input output stream is a preprocessor command
+#include <cmath>
 
 using std::cout;
+using std::cin;
+using std::endl;
 
-int main() //main function int is short for integer related to return 0
+int multiply(int x, int y); // Need prototypes function declaration if you don't define the function before using the function
+
+//The parenthesis represent input to a function a.k.a parameters which is give arguments
+int main() //main function int is short for integer/related to return 0 called automatically
 {
-    int slices = 5;
-    cout << slices; //statement or steps end with ;
+    int slices;
+    cout << "YO bro how many slices of pizza did you eat?" << endl;
+    cin >> slices;
+    cout << "You have " << slices << " slices of pizza" << endl;
+    cout << pow(10,2)*2 << endl; //argument is data passed into a function
+    cout << multiply(5,6) << endl; //Function call
 }
 
 
+//Function defintion
+int multiply(int x, int y)
+{
+    return x * y;
+}
+
+
+//arrows point in the way of the data flow
 // (<<) is an operator and means that you will operate on the following operand
 // cout === console out
 // cin  === console in
