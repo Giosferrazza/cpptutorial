1929ec746f3f9626a30098b21578520b6ed50014
Giovanni Sferrazza <giosferrazza12@gmail.com>
2024-03-19 23:22:28 -0700

    Modularization of functions!

--- youtube.cpp
+++ youtube.cpp
@@ -6,14 +6,24 @@ using std::cin;
 using std::endl;
 
 double power(double, int); // Need prototypes function declaration// Only use data types no var 
-
+void print_pow(double, int);
 //The parenthesis represent input to a function a.k.a parameters which is give arguments
 int main() //main function int is short for integer/related to return 0 called automatically
-{
-    cout << power(1,0) << endl;
+{    
+    int base, exponent;
+    cout << "What is the base?: ";
+    cin >> base;
+    cout << "What is the exponent?: ";
+    cin >> exponent;
+    print_pow(base, exponent);
 }
 
-double power(double base, int exponent)
+void print_pow(double base, int exponent) //print the function and handle user input as well!
+{
+    cout << base << " raised to the " << exponent << " is = " << power(base, exponent) << endl;
+}
+
+double power(double base, int exponent) //Math for the POWER function
 {
     double result = 1;
     for(int i = 0; i < exponent; i++) // dont forget to increment the index
